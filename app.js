a6835ad5f54084e6ee0ea26df28b367b19dc35bd
iampradipta <128884944+iampradipta@users.noreply.github.com>
2023-04-26 11:46:32 +0530

    banana talk ready

---
+++ app.js
@@ -0,0 +1,12 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput= document.querySelector("#txt-input")
+var txtOutput= document.querySelector("#txt-output")
+
+
+
+function clickHandler(){
+   
+     txtOutput.innerText = "ajajaja "+txtInput.value
+}
+
+//btnTranslate.addEventListener("click", clickHandler)
